0e802dd1c636992a21edab85f89d0973b63f437e
saxcda <ericli9222@gmail.com>
2021-07-13 05:05:17 +0800

    change data to dataframe, reformat the dataframe

--- Automatic_Stock_Trading_Machine/Main.py
+++ Automatic_Stock_Trading_Machine/Main.py
@@ -1,5 +1,9 @@
 import requests
 import urllib3
+import json
+import numpy as np
+import pandas as pd
+import datetime
 
 '''
 Main machine
@@ -21,14 +25,27 @@ class Machine():
         self.get_stock()
 
     def get_stock(self):
-        #use 2330.TW 
+        #test 2330.TW 
+        codaname = ""
+        epoch_time_now = str(int(datetime.datetime.now().timestamp()))
         self.header = {"..."}
         self.url = "..."
 
-        html = requests.get(self.url, verify=False, headers=self.header)
-        html.encoding="utf-8"
+        self.html = requests.get(self.url, verify=False, headers=self.header)
+        #self.html.encoding="utf-8"
+        #self.html.text[:1000]
 
-        print(html.text)
+        self.data = json.loads(self.html.text)
+        
+        self.tmp_list = self.data['chart']['result'][0]['indicators']['quote'][0]
+        self.df = pd.DataFrame({'open'  : self.tmp_list['open'],
+                                'close' : self.tmp_list['close'],
+                                'low'   : self.tmp_list['low'],
+                                'high'  : self.tmp_list['high'],
+                                'volume': self.tmp_list['volume']},
+                                index = (np.array([datetime.datetime.utcfromtimestamp(x) for x in self.data['chart']['result'][0]['timestamp']])))
+
+        print(self.df)
 
 if __name__ == '__main__':
     machine = Machine()
--- Automatic_Stock_Trading_Machine/test.py
+++ Automatic_Stock_Trading_Machine/test.py
@@ -1,30 +1,68 @@
-import requests
-import urllib3
-import numpy as np
+'''
+{"chart":
+    {"result":
+        [{"meta":
+            {"currency":null,
+             "symbol":"APPL",
+             "exchangeName":"YHD",
+             "instrumentType":"MUTUALFUND",
+             "firstTradeDate":null,
+             "regularMarketTime":1561759658,
+             "gmtoffset":-14400,
+             "timezone":"EDT",
+             "exchangeTimezoneName":"America/New_York",
+             "priceHint":2,
+             "currentTradingPeriod":
+                {"pre":{"timezone":"EDT","start":1625817600,"end":1625837400,"gmtoffset":-14400},
+                 "regular":{"timezone":"EDT","start":1625837400,"end":1625860800,"gmtoffset":-14400},
+                 "post":{"timezone":"EDT","start":1625860800,"end":1625875200,"gmtoffset":-14400}
+                },
+             "dataGranularity":"1d",
+             "range":"",
+             "validRanges":["1mo","3mo","6mo","ytd","1y","2y","5y","10y","max"]
 
-urllib3.disable_warnings()
+            },
+            "indicators":{"quote":[{}],"adjclose":[{}]}}
 
-class Machine():
-    def __init__(self):
-        self.header = {}
-        self.url = ""
+        ],
+        "error":null
 
-        self.initialize()
-        
-    
-    def initialize(self):
+    }
+}
 
-        self.get_stock()
+{'chart': 
+    {'result': 
+        [{'meta': 
+            {'currency': 'TWD', 
+            'symbol': '2330.TW', 
+            'exchangeName': 'TAI', 
+            'instrumentType': 'EQUITY', 
+            'firstTradeDate': 946947600, 
+            'regularMarketTime': 1625808608, 
+            'gmtoffset': 28800, 
+            'timezone': 'CST', 
+            'exchangeTimezoneName': 'Asia/Taipei', 
+            'regularMarketPrice': 584.0, 
+            'chartPreviousClose': 69.649, 
+            'priceHint': 2, 
+            'currentTradingPeriod': 
+                {'pre': {'timezone': 'CST', 'start': 1626051600, 'end': 1626051600, 'gmtoffset': 28800}, 
+                'regular': {'timezone': 'CST', 'start': 1626051600, 'end': 1626067800, 'gmtoffset': 28800}, 
+                'post': {'timezone': 'CST', 'start': 1626067800, 'end': 1626067800, 'gmtoffset': 28800}
+                }, 
+            'dataGranularity': '1d', 
+            'range': '', 
+            'validRanges': ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']}, 
+            'timestamp': [], 
+            'indicators': {'quote': [{'open': [], 'high': [], 'low': [], 'close': [], 'volume': []}], 
+            'adjclose': [{'adjclose': []}]}}], 
+            'error': None
+            
+    }
+            
+}
 
-    def get_stock(self):
-        self.header = {"..."}
-        self.url = "..."
+index=(np.array(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime((self.data['chart']['result'][0]['timestamp']))))))
 
-        html = requests.get(self.url, verify=False, headers=self.header)
-        html.encoding="utf-8"
+'''
 
-        print(html.text)
-
-if __name__ == '__main__':
-    machine = Machine()
-    print("test")
